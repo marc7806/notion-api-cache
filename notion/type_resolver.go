eb2189cc9561cf9bc84f7f0d01ff7bd546d0357c
Marc <marc7806@gmx.de>
2022-05-22 18:45:32 +0200

    Add more notion type resolvers

--- notion/type_resolver.go
+++ notion/type_resolver.go
@@ -1,16 +1,6 @@
 package notion
 
-import (
-	"fmt"
-)
-
-type RichText struct {
-	Type        string      `json:"type"`
-	Text        interface{} `json:"text"`
-	Annotations interface{} `json:"annotations"`
-	PlainText   string      `json:"plain_text"`
-	Href        interface{} `json:"href"`
-}
+import "log"
 
 type fn func(map[string]interface{}, string) interface{}
 
@@ -19,9 +9,15 @@ var (
 )
 
 func init() {
-	fmt.Println("Initializinggg")
+	log.Println("Initializing Notion property type resolvers")
 	resolvers = map[string]fn{
-		"rich_text": resolveRichText,
+		"rich_text":        resolvePlainText,
+		"title":            resolvePlainText,
+		"last_edited_time": resolveLastEditedTime,
+		"created_time":     resolveCreatedTime,
+		"select":           resolveSelect,
+		"multi_select":     resolveMultiSelect,
+		"formula":          resolveFormula,
 	}
 }
 
@@ -31,10 +27,10 @@ func ResolvePropertyType(property map[string]interface{}) interface{} {
 	if ok {
 		return resolver(property, propType)
 	}
-	return "EMPTY"
+	return nil
 }
 
-func resolveRichText(prop map[string]interface{}, propType string) interface{} {
+func resolvePlainText(prop map[string]interface{}, propType string) interface{} {
 	richTextData := prop[propType].([]interface{})
 	result := ""
 
@@ -46,4 +42,39 @@ func resolveRichText(prop map[string]interface{}, propType string) interface{} {
 		result += dataEntry.(map[string]interface{})["plain_text"].(string)
 	}
 	return result
+}
+
+func resolveLastEditedTime(prop map[string]interface{}, propType string) interface{} {
+	return prop[propType]
+}
+
+func resolveCreatedTime(prop map[string]interface{}, propType string) interface{} {
+	return prop[propType]
+}
+
+func resolveSelect(prop map[string]interface{}, propType string) interface{} {
+	selectData := prop[propType]
+	if selectData == nil {
+		return ""
+	}
+	return selectData.(map[string]interface{})["name"]
+}
+
+func resolveMultiSelect(prop map[string]interface{}, propType string) interface{} {
+	multiSelectOptions := prop[propType].([]interface{})
+	result := ""
+
+	if len(multiSelectOptions) == 0 {
+		return result
+	}
+
+	for _, dataEntry := range multiSelectOptions {
+		// currently we concat multi values with ; seperator to a string
+		result += dataEntry.(map[string]interface{})["name"].(string) + ";"
+	}
+	return result
+}
+
+func resolveFormula(prop map[string]interface{}, propType string) interface{} {
+	return prop[propType].(map[string]interface{})["string"]
 }
