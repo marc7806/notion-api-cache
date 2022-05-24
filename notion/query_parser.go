7eb2ed0d01eaa5cd03e17d7399fbcdb0667d63d3
Marc <marc7806@gmx.de>
2022-05-24 23:37:45 +0200

    Fix query parser

--- notion/query_parser.go
+++ notion/query_parser.go
@@ -1,5 +1,12 @@
 package notion
 
+import (
+	"encoding/json"
+	"errors"
+	"reflect"
+	"strconv"
+)
+
 type FilterTreeNode struct {
 	CompoundType *CompoundFilterType
 	Operation    *FilterOperation
@@ -15,28 +22,35 @@ type FilterOperation struct {
 type CompoundFilterType string
 
 const (
-	Leaf CompoundFilterType = "leaf"
-	And                     = "and"
-	Or                      = "or"
+	And CompoundFilterType = "and"
+	Or                     = "or"
 )
 
-type FilterCondition int64
+type FilterCondition string
 
 const (
-	Equals FilterCondition = iota
-	IsNotEmpty
+	Equals         FilterCondition = "equals"
+	DoesNotEqual                   = "does_not_equal"
+	Contains                       = "contains"
+	DoesNotContain                 = "does_not_contain"
+	StartsWith                     = "starts_with"
+	EndsWith                       = "ends_with"
+	IsEmpty                        = "is_empty"
+	IsNotEmpty                     = "is_not_empty"
 )
 
 func CreateFilterTree(filterQuery map[string]interface{}) *FilterTreeNode {
 	// recursive parse query body from rest endpoint using dfs and return filter tree node
-	var filterTreeNode *FilterTreeNode
-	return traverseFilterQuery(filterQuery, filterTreeNode)
+	return traverseFilterQuery(filterQuery)
 }
 
-func traverseFilterQuery(filterQuery map[string]interface{}, filterTreeNode *FilterTreeNode) *FilterTreeNode {
+func traverseFilterQuery(filterQuery map[string]interface{}) *FilterTreeNode {
 	// 1. case: single property filter - Basecase
 	if isPropertyField(filterQuery) {
-		condition, value := resolveFilterCondition(filterQuery)
+		condition, value, err := resolveFilterCondition(filterQuery)
+		if err != nil {
+			return nil
+		}
 		filterOp := FilterOperation{
 			Property:  filterQuery["property"].(string),
 			Condition: condition,
@@ -49,19 +63,32 @@ func traverseFilterQuery(filterQuery map[string]interface{}, filterTreeNode *Fil
 
 	// 2. case: nested multi filter
 	filterCompoundType := resolveCompoundFilterType(filterQuery)
-	if filterTreeNode == nil {
-		filterTreeNode = &FilterTreeNode{
-			CompoundType: &filterCompoundType,
-		}
+	filterTreeNode := &FilterTreeNode{
+		CompoundType: &filterCompoundType,
 	}
 	for _, el := range filterQuery[string(filterCompoundType)].([]interface{}) {
-		filterTreeNode.Children = append(filterTreeNode.Children, *traverseFilterQuery(el.(map[string]interface{}), filterTreeNode))
+		filterTreeNode.Children = append(filterTreeNode.Children, *traverseFilterQuery(el.(map[string]interface{})))
 	}
 	return filterTreeNode
 }
 
-func resolveFilterCondition(propertyFilterObject map[string]interface{}) (condition FilterCondition, value string) {
-	return Equals, "HELLO WORLD"
+func resolveFilterCondition(propertyFilterObject map[string]interface{}) (condition FilterCondition, value string, err error) {
+	for key, val := range propertyFilterObject {
+		if key != "property" {
+			// here we have our filter condition
+			for conditionKey, conditionVal := range val.(map[string]interface{}) {
+				convertedVal := ""
+				if reflect.TypeOf(conditionVal).Kind() == reflect.Bool {
+					convertedVal = strconv.FormatBool(conditionVal.(bool))
+				} else {
+					convertedVal = conditionVal.(string)
+				}
+				return FilterCondition(conditionKey), convertedVal, nil
+			}
+		}
+	}
+	propString, _ := json.Marshal(propertyFilterObject)
+	return Equals, "", errors.New("Error: Query contains not supported filter condition: " + string(propString))
 }
 
 func resolveCompoundFilterType(compoundFilterObject map[string]interface{}) CompoundFilterType {
